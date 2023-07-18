65473978aef45010bc3afbddbb9f533d1a724388
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 11:29:23 +0200

    alpine in

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './style.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<button type="button" class="btn-primary">Copy</button>
-<button type="button" class="btn-primary">Send</button>
-<button type="button" class="btn-primary">Save</button>
-<button type="button" class="btn-danger">Delete</button>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+<div x-data="{message: ''}" class="flex items-start">
+	<button class="btn-primary" @click="message = 'item copied'">Copy</button>
+	<button class="btn-primary" @click="message = 'item sent'">Send</button>
+	<button class="btn-primary" @click="message = 'item saved'">Save</button>
+	<button class="btn-danger" @click="message = 'item deleted'">Delete</button>
+	<div class="text-gray-400 text-4xl grid items-center" x-text="message"></div>
+</div>
 `
