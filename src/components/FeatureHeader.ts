9f20317ba862a30dfd5c45a142c727d28e720895
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 11:34:07 +0200

    feature header

---
+++ src/components/FeatureHeader.ts
@@ -0,0 +1,5 @@
+export const FeatureHeader = (text: string) => {
+	return `
+<h2 class="text-2xl mb-4 text-yellow-600">${text}</h2>
+`;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { FeatureHeader } from './components/FeatureHeader'
 import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+${FeatureHeader('Interactive Buttons')}
 <div x-data="{message: ''}" class="flex items-start">
 	<button class="btn-primary" @click="message = 'item copied'">Copy</button>
 	<button class="btn-primary" @click="message = 'item sent'">Send</button>
@@ -8,4 +10,10 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
 	<button class="btn-danger" @click="message = 'item deleted'">Delete</button>
 	<div class="text-gray-400 text-4xl grid items-center" x-text="message"></div>
 </div>
+
+${FeatureHeader('Employee Cards')}
+
+${FeatureHeader('Feature 3')}
+
+${FeatureHeader('Feature 4')}
 `
