68e9aabd1e3bda05b62a78676132215cc9ae3072
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 11:16:51 +0200

    apply buttons

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-    <main class="grid place-items-center">
-        <h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
-			  <h2>test</h2>
-        <p class="text-3xl mt-7 italic w-1/2 leading-10">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
-    </main>
+<button type="button" class="btn-primary">Copy</button>
+<button type="button" class="btn-primary">Send</button>
+<button type="button" class="btn-primary">Save</button>
+<button type="button" class="btn-danger">Delete</button>
 `
