4a4a9cbbda4cfbef204844069d277d25ac205e4a
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 11:03:34 +0200

    sass installed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import './style.css'
+import './style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <main class="grid place-items-center">
         <h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
+			  <h2>test</h2>
         <p class="text-3xl mt-7 italic w-1/2 leading-10">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
     </main>
 `
