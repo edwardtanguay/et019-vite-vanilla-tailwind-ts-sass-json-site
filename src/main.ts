9bb63125b14029799425a83b9d809ee19786c99d
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-18 10:57:34 +0200

    site styled

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<h1>Vite-Vanilla Tailwind</h1>
-<p>testing</p>
+    <main class="grid place-items-center">
+        <h1 class="text-green-800 text-7xl">This is a test of Tailwind</h1>
+        <p class="text-3xl mt-7 italic w-1/2 leading-10">Lorem ipsum, dolor sit <span class="highlight">amet</span> consectetur adipisicing elit. Ab consequatur, et natus quis at repellendus rerum quas! Atque fuga repudiandae laborum <span class="highlight">eveniet</span> facilis, harum quas dignissimos <span class="highlight">okisdje</span>? Deleniti, ipsam?</p>
+    </main>
 `
